562a3e5a49de77809ec08840fe13b78557e1d108
nono <nono.polkadot@gmail.com>
2023-11-08 10:48:12 +0800

    create navigation bar ui

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
 			<footer className="bg-white border-t">
 				<div className="mx-auto py-10">
 					<p className="text-center text-xs text-black">
-						&copy; 2023 Nono, Inc. All right reserved
+						&copy; 2023 Nono, Inc. All rights reserved
 					</p>
 				</div>
 			</footer>
