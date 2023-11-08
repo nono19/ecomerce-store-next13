554509964ad84c5f9508caa42778839a12594364
nono <nono.polkadot@gmail.com>
2023-11-08 13:27:56 +0800

    add more type on types.ts

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -5,7 +5,7 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const billboard = await getBillboard("56be7164-786f-4632-8a67-c571147f74ac");
+  const billboard = await getBillboard("ba9509ef-f164-4ef1-877d-e3a8c568c840");
   return (
     <Container>
       <div className="space-y-10 pb-10">
