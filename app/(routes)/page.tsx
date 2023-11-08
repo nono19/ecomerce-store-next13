8f25bd1af6faeae16852af6b5cbe5c3fd32f4264
nono <nono.polkadot@gmail.com>
2023-11-08 11:46:24 +0800

    create billboard feature

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,17 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboards";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("56be7164-786f-4632-8a67-c571147f74ac");
   return (
-    <div>page</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   )
 }
 
